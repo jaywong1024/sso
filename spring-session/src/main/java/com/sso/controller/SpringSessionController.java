cc531d0eb33f6b2a9b8cb7d93f907512e2cbd4fc
ChinesePowerful <36500407+ChinesePowerful@users.noreply.github.com>
2019-05-23 15:53:50 +0800

    for win

--- spring-session/src/main/java/com/sso/controller/SpringSessionController.java
+++ spring-session/src/main/java/com/sso/controller/SpringSessionController.java
@@ -1,4 +1,22 @@
 package com.sso.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
 public class SpringSessionController {
+
+    @GetMapping(value = "/test")
+    public String springSession(HttpServletRequest request, HttpServletResponse response) {
+        Object attributeValue = request.getSession().getAttribute("testAttributeName");
+        if (null == attributeValue) {
+            request.getSession().setAttribute("testAttributeName", "testAttributeValue");
+        }
+        System.out.println("80: " + attributeValue);
+        return "spring-session";
+    }
+
 }
---
+++ spring-session/src/main/webapp/WEB-INF/jsp/spring-session.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jiqunsoftpc4
+  Date: 2019/5/23
+  Time: 15:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>SpringSession</title>
+    <style>
+        *{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>SpringSession</h1>
+</body>
+</html>
