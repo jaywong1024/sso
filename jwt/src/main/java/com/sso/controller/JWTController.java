f5694231473db0298bb85ba2ca9e087849743395
ChinesePowerful <36500407+ChinesePowerful@users.noreply.github.com>
2019-05-27 17:31:49 +0800

    for win

--- jwt/src/main/java/com/sso/controller/JWTController.java
+++ jwt/src/main/java/com/sso/controller/JWTController.java
@@ -1,21 +1,73 @@
 package com.sso.controller;
 
-import com.sso.commons.JWTResponseData;
+import com.sso.commons.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class JWTController {
 
+    /**
+     * 验证客户端登录，如果用户名和密码正确则创建一个 Token令牌返回给客户端，客户端发送请求时必需携带此 Token
+     * @param username 账号
+     * @param password 密码
+     * @return jwtResponseData(状态码，业务数据，描述信息，Token令牌)
+     */
     @ResponseBody
     @PostMapping(value = "/login")
     public Object login(String username, String password) {
         JWTResponseData jwtResponseData = null;
-
-
-
+//        校验账号密码，这里使用的是 JWTUsers 静态数据来模拟数据库
+        if (JWTUsers.isLogin(username, password)) {
+            JWTSubject jwtSubject = new JWTSubject(username);
+//            创建 token 令牌：使用 UUID 为一个客户端提供一个唯一标记，签发者 cp-test-jwt，JSON格式 的主题，有效时长 60s
+            String jwtToken = JWTUtils.createJWT(UUID.randomUUID().toString(),
+                    "cp-test-jwt", JWTUtils.generateSubject(jwtSubject), (1 * 60 * 1000));
+            jwtResponseData = new JWTResponseData();
+            jwtResponseData.setCode(200);
+            jwtResponseData.setData(null);
+            jwtResponseData.setMessage("登陆成功");
+            jwtResponseData.setToken(jwtToken);
+        } else {
+            jwtResponseData = new JWTResponseData();
+            jwtResponseData.setCode(500);
+            jwtResponseData.setData(null);
+            jwtResponseData.setMessage("登录失败");
+            jwtResponseData.setToken(null);
+        }
         return jwtResponseData;
     }
 
+    /**
+     * 校验客户端发送请求时携带的 Token令牌
+     * @param request 请求
+     * @return jwtResponseData(状态码，业务数据，描述信息，Token令牌)
+     */
+    @ResponseBody
+    @PostMapping(value = "/validateJWT")
+    public Object validateJWT(HttpServletRequest request) {
+//        获取客户端携带的 Token令牌
+        String token = request.getHeader("Authorization");
+//        校验 Token令牌
+        JWTResult jwtResult = JWTUtils.validateJWT(token);
+        JWTResponseData jwtResponseData = new JWTResponseData();
+//        根据校验结果返回信息
+        if (jwtResult.isSuccess()) {
+            jwtResponseData.setCode(200);
+            jwtResponseData.setData(jwtResult.getClaims().getSubject());
+//            返回一个新的 Token令牌
+            String newToken = JWTUtils.createJWT(jwtResult.getClaims().getId(), jwtResult.getClaims().getIssuer(),
+                    jwtResult.getClaims().getSubject(), (1 * 60 * 1000));
+            jwtResponseData.setToken(newToken);
+            return jwtResponseData;
+        } else {
+            jwtResponseData.setCode(500);
+            jwtResponseData.setMessage("用户未登录");
+            return jwtResponseData;
+        }
+    }
 }
--- jwt/src/main/webapp/WEB-INF/jsp/login.jsp
+++ jwt/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>登录界面</title>
+    <script src="webjars/jquery/3.3.1-1/jquery.js"></script>
     <style>
         *{
             text-align: center;
@@ -12,12 +13,28 @@
     <div>
         <h3>登录测试</h3>
         <form action="" method="post">
-            username: <input type="text" name="username">
-            password: <input type="text" name="password">
-            <input type="submit" value="登录">
+            username: <input type="text" name="username" id="username">
+            password: <input type="text" name="password" id="password">
+            <input type="submit" value="登录" id="submit_login">
         </form>
         <h3>测试 Token 有效性</h3>
-        <button>Test LocalStorage</button>
+        <button id="button_test_localStorage">Test LocalStorage</button>
     </div>
+    <script>
+        $(function () {
+            $('#submit_login').click(function () {
+                // $.post('/login', {
+                //     username: $('#username').val(),
+                //     password: $('#password').val()
+                // }, function (result) {
+                //     console.log(result);
+                // });
+                alert('login');
+            });
+            $('#button_test_localStorage').click(function () {
+                alert('test');
+            });
+        });
+    </script>
 </body>
 </html>
