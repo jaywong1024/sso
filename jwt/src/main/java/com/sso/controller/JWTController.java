8889b2f5a54d1f0cb494fbea92e4d62c023fc411
ChinesePowerful <36500407+ChinesePowerful@users.noreply.github.com>
2019-05-27 16:35:34 +0800

    for win

--- jwt/src/main/java/com/sso/controller/JWTController.java
+++ jwt/src/main/java/com/sso/controller/JWTController.java
@@ -1,10 +1,21 @@
 package com.sso.controller;
 
+import com.sso.commons.JWTResponseData;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JWTController {
 
+    @ResponseBody
+    @PostMapping(value = "/login")
+    public Object login(String username, String password) {
+        JWTResponseData jwtResponseData = null;
 
+
+
+        return jwtResponseData;
+    }
 
 }
--- jwt/src/main/java/com/sso/controller/PageController.java
+++ jwt/src/main/java/com/sso/controller/PageController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping(value = "/login")
-    public String login() {
-        System.out.println("pageController.login");
+    @GetMapping(value = "/")
+    public String loginPage() {
+        System.out.println("pageController.loginPage");
         return "login";
     }
 
